0c9a17aca494a8f0283e2d17a9ff789a178e32be
Hibiki Ono <onohibiki0427@gmail.com>
2025-03-14 21:18:52 +0900

    Initial commit

---
+++ src/test/java/com/HibikiOno/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HibikiOno.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
